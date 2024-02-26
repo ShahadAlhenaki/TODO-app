90547aff84a9d531b2644541252cc8f67458de2a
shahad alhenaki <shahadkh00@gmail.com>
2024-02-26 18:31:54 +0300

    add-everything

--- index.js
+++ index.js
@@ -1,116 +1,110 @@
-let todos = [{
-   title: "Do assignment",
-   description:"JS assignment before midnight",
-},
-   {  
-    title: "Do laundry", 
-    description:" wash black clothes",
-},
-   { 
-    title:"Play with my cat",
-    description:"12 min",
-   }
-]
+let todos = [
+  {
+    title: "Do assignment",
+    description: "JS assignment before midnight",
+  },
+  {
+    title: "Do laundry",
+    description: " wash black clothes",
+  },
+  {
+    title: "Play with my cat",
+    description: "12 min",
+  },
+];
 
-const form = document.getElementById("add-todo")
-const deleteAllBtn = document.getElementById("delete-all")
-const ul = document.createElement("ul")
-
+const form = document.getElementById("add-todo");
+const deleteAllBtn = document.getElementById("delete-all");
+const ul = document.createElement("ul");
 
 function renderTodos(array) {
-ul.innerHTML = ""
-const container = document.querySelector(".container")
+  ul.innerHTML = "";
+  const container = document.querySelector(".container");
 
-ul.classList.add("todos")
-container.appendChild(ul)
+  ul.classList.add("todos");
+  container.appendChild(ul);
 
-array.forEach((task) => {
-    const li = document.createElement("li")
-    const title = document.createElement("span")
-    const description = document.createElement("p")
-    const checkbox = document.createElement("input")
-    checkbox.type = "checkbox"
+  array.forEach((task) => {
+    const li = document.createElement("li");
+    const title = document.createElement("span");
+    const description = document.createElement("p");
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
 
-    const deleteBtn = document.createElement("button")
-    deleteBtn.textContent = "Delete"
-    deleteBtn.classList.add("btn", "delete-btn")
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete";
+    deleteBtn.classList.add("btn", "delete-btn");
     deleteBtn.addEventListener("click", (e) => {
-       const todo = e.target.parentNode
-       const selectedTodo = todo.querySelector("span").textContent
+      const todo = e.target.parentNode;
+      const selectedTodo = todo.querySelector("span").textContent;
 
-       
-        removeTodo(selectedTodo)
-    })
+      removeTodo(selectedTodo);
+    });
 
-    li.classList.add("todo")
-    title.textContent = task.title
-    description.textContent = task.description
+    li.classList.add("todo");
+    title.textContent = task.title;
+    description.textContent = task.description;
 
-    li.append(title, description)
-    li.appendChild(deleteBtn)
-    li.appendChild(checkbox)
-    ul.appendChild(li)
-})
+    li.append(title, description);
+    li.appendChild(deleteBtn);
+    li.appendChild(checkbox);
+    ul.appendChild(li);
+  });
 }
-function addTodo(value){
-    const ul = document.querySelector(".todos")
-    const li = document.createElement("li")
-    const title = document.createElement("span")
-    const  description = document.createElement("p")
-    const deleteBtn = document.createElement("button")
-    const todo = {
-        title: value,
-        description:"to be ",
-    }
-    const checkbox = document.createElement("input")
-    checkbox.type = "checkbox"
+function addTodo(value) {
+  const ul = document.querySelector(".todos");
+  const li = document.createElement("li");
+  const title = document.createElement("span");
+  const description = document.createElement("p");
+  const deleteBtn = document.createElement("button");
+  const todo = {
+    title: value,
+    description: "to be ",
+  };
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
 
-    deleteBtn.textContent = "Delete"
-    deleteBtn.classList.add("btn", "delete-btn")
+  deleteBtn.textContent = "Delete";
+  deleteBtn.classList.add("btn", "delete-btn");
 
-    deleteBtn.addEventListener("click", (e) => {
-       const todo = e.target.parentNode
-       const selectedTodo = todo.querySelector("span").textContent
+  deleteBtn.addEventListener("click", (e) => {
+    const todo = e.target.parentNode;
+    const selectedTodo = todo.querySelector("span").textContent;
 
-       removeTodo(selectedTodo)
-    })
+    removeTodo(selectedTodo);
+  });
 
-    todos.push(todo)
+  todos.push(todo);
 
-    title.textContent = todo.title
-    description.textContent = todo.description
-    li.classList.add("todo")
+  title.textContent = todo.title;
+  description.textContent = todo.description;
+  li.classList.add("todo");
 
-    li.append(title, description, deleteBtn)
-    li.appendChild(checkbox)
-    ul.appendChild(li)
+  li.append(title, description, deleteBtn);
+  li.appendChild(checkbox);
+  ul.appendChild(li);
 }
 
 function removeTodo(selectedTodo) {
-const filteredTodo = todos.filter((todo) => {
-return todo.title !== selectedTodo
-})
-todos = filteredTodo
-renderTodos(filteredTodo)
+  const filteredTodo = todos.filter((todo) => {
+    return todo.title !== selectedTodo;
+  });
+  todos = filteredTodo;
+  renderTodos(filteredTodo);
 }
 
-form.addEventListener("submit" , (e) => {
-const value = document.querySelector(".todo-input").value
-addTodo(value)
+form.addEventListener("submit", (e) => {
+  const value = document.querySelector(".todo-input").value;
+  addTodo(value);
 
-e.preventDefault()
-})
+  e.preventDefault();
+});
 
 deleteAllBtn.addEventListener("click", () => {
-todos = []
-renderTodos(todos)
-})
+  todos = [];
+  renderTodos(todos);
+});
 
 window.addEventListener("load", () => {
-    renderTodos(todos)
-})
-
-
-
-
- 
+  renderTodos(todos);
+});
